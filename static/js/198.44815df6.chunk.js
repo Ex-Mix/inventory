a952ee669f7ad80b8b932a0278c454bc2cdbcebd
Ex-Mix <144747150+Ex-Mix@users.noreply.github.com>
2025-03-10 07:28:18 +0000

    Deploying to gh-pages from @ Ex-Mix/inventory@25c65b2c25f5cd0e488a8fec0c51a602735e8a05 🚀

---
+++ static/js/198.44815df6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmaterial_dashboard_2_react=self.webpackChunkmaterial_dashboard_2_react||[]).push([[198],{1198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.44815df6.chunk.js.map
